4132e99aca133903577b53ee39b44967ed46b2c2
zengfanrong <2376094638@qq.com>
2019-07-22 17:23:49 +0800

    mybatis

--- code_project/spring/src/main/java/com/zfr/aaron/spring/Application.java
+++ code_project/spring/src/main/java/com/zfr/aaron/spring/Application.java
@@ -1,9 +1,15 @@
 package com.zfr.aaron.spring;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zfr
+ * 启动类
+ */
 @SpringBootApplication
+@MapperScan({"com.zfr.aaron.spring.mapper"})
 public class Application {
 
 	public static void main(String[] args) {
